03e7535ed93d6fa1f9582b64d84d9915e90466fe
danilalabydindfsdf <labydin2605@mail.ru>
2022-03-13 21:00:00 +0300

    add some data

--- char12_bytes_batearray.py
+++ char12_bytes_batearray.py
@@ -12,6 +12,17 @@ def main():
     except TypeError as err:
         print(err)
 
+    # but a bytearray is mutable
+    blist = [1, 2, 4, 255]
+    the_byte_array = bytearray(blist)
+    print(the_byte_array)
+    the_byte_array[1] = 127
+    print(the_byte_array)
+
+    # each of this variables can contain a value(0-255) and max len = 256
+    the_bytes = bytes(range(0, 256))
+    the_byte_array = bytearray(range(0, 256))
+    print(the_bytes)
 
 if __name__ == '__main__':
     main()
